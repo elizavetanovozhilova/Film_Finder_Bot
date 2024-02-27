fed1e76585b698adb5f5e11dddee854e7cebceb5
elizavetanovozilova <babygorilla@yandex.ru>
2024-02-28 01:31:56 +0300

    added random film

--- handlers.py
+++ handlers.py
@@ -4,6 +4,8 @@ from aiogram.types import Message
 import keyboard
 import text
 router = Router()
+from random import choice
+
 
 @router.message(Command("start"))
 async def start_handler(message: Message):
@@ -14,3 +16,14 @@ async def start_handler(message: Message):
 @router.message(F.text == "◀️ Выйти в меню")
 async def menu(message: Message):
     await message.answer(text.menu, reply_markup=keyboard.menu)
+
+'''
+@dp.callback_query(F.data == "film_info")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer()
+'''
+
+films=['Star Wars', 'Barbie', 'Interstellar', 'Snitch']
+@router.callback_query(F.data == "random_film")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer(str(choice(films)))
